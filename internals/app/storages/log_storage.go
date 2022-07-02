de807455954cbc1812d12f5bf0ff7427c4ffb6e8
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2022-07-02 22:48:46 +0300

    Move writing feature under processors scope

--- internals/app/storages/log_storage.go
+++ internals/app/storages/log_storage.go
@@ -1,7 +1,6 @@
 package storages
 
 import (
-	"fmt"
 	"log"
 	"os"
 )
@@ -27,12 +26,4 @@ func NewLogStorage(path string) *LogStorage {
 	}
 
 	return storage
-}
-
-func (storage *LogStorage) WriteLog(input string) {
-	res, err := storage.Storage.WriteString(input)
-	if err != nil {
-		log.Fatalln("failed to write into file:", err)
-	}
-	fmt.Fprintf(os.Stdout, "write %d bytes\n", res)
 }
