b59cbd0a4b9bfbc154928b4b73474c4893f2fe9c
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2022-07-02 22:33:53 +0300

    Manage envs via config constructor

--- internals/cfg/config.go
+++ internals/cfg/config.go
@@ -3,18 +3,25 @@ package cfg
 import "os"
 
 type Config struct {
-	Port         string
-	DefaultPath  string
-	ProvidedPath string
+	Port        string
+	LogFilePath string
 }
 
 func NewConfig() *Config {
 	cfg := new(Config)
+	cfg.SetUpEnv()
 
-	cfg.DefaultPath = "./log.txt"
-	if path, ok := os.LookupEnv("APP_LOGFILE_PATH"); ok {
-		cfg.ProvidedPath = path
+	cfg.Port = os.Getenv("APP_DEFAULT_PORT")
+	cfg.LogFilePath = os.Getenv("APP_DEFAULT_LOGFILE")
+
+	if _, ok := os.LookupEnv("APP_LOGFILE_PATH"); ok {
+		cfg.LogFilePath = os.Getenv("APP_LOGFILE_PATH")
 	}
 
 	return cfg
+}
+
+func (cfg *Config) SetUpEnv() {
+	os.Setenv("APP_DEFAULT_PORT", "8080")
+	os.Setenv("APP_DEFAULT_LOGFILE", "./log.txt")
 }
